24f330c24920c8ebc1f7a88b7200bfafe2c63cb7
Dshane Godbold <dshanegodbold@gmail.com>
2022-02-09 11:42:00 -0600

    update

--- main.js
+++ main.js
@@ -1,15 +1,28 @@
-const names = ["Lisa", "Dshane", "Kati", "Parish", "Abbi"];
+import got from "got";
 
+/**
+ * Get a user's repos off of GitHub
+ * @param {string} username
+ * @param {function} callback This will return an error and an array of repositories
+ * Documentation: https://docs.github.com/en/rest/reference/repos#list-repositories-for-a-user
+ */
+async function github(username, callback) {
+  let err, data;
+  try {
+    const resp = got(`https://api.github.com/users/${username}/repos`);
+    data = await resp.json();
+    callback(err, data);
+  } catch (err) {
+    callback(err, data);
+  }
+}
 function main() {
-  const sorted = names.sort(function compare(a,b) {
-    if (a<b) {
-      return -1;
-    } else if (b<a) {
-      return 1;
-    } 
-    return 0;
+  github("dshaney", (error, projects) => {
+      for ( let project of projects){
+
+    console.log(`${project.name}, has ${project.description}, has ${project.stargazers_count}`);
+      }
   });
-  console.log(sorted);
 }
 
 main();
